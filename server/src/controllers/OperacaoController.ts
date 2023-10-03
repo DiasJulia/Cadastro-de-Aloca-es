4794fee2bb6569293e14c075a91f966e15b71fef
diasjulia <jedl@cin.ufpe.br>
2023-10-03 16:45:08 -0300

    feat: operacao Create and Read functions

--- server/src/controllers/OperacaoController.ts
+++ server/src/controllers/OperacaoController.ts
@@ -1,6 +1,50 @@
 import { Request, Response } from "express";
 import { Operacao } from "../models/Operacao";
+import { AppDataSource } from "../data-source";
 
-class OperacaoController {}
+class OperacaoController {
+  async create(req: Request, res: Response) {
+    try {
+      const { CNPJ, razao_social, tipo, data, quantidade, valor } = req.body;
+
+      const operacao = new Operacao();
+      operacao.CNPJ = CNPJ;
+      operacao.razao_social = razao_social;
+      operacao.tipo = tipo;
+      operacao.data = new Date(data);
+      operacao.quantidade = quantidade;
+      operacao.valor = valor;
+
+      await AppDataSource.manager.save(operacao);
+      res.status(201).json(operacao);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async readAll(req: Request, res: Response) {
+    try {
+      const operacoes = await AppDataSource.manager.find(Operacao);
+      res.json(operacoes);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async readByCNPJ(req: Request, res: Response) {
+    try {
+      const { CNPJ } = req.params;
+      const operacoes = await AppDataSource.manager.find(Operacao, {
+        where: { CNPJ },
+      });
+      res.json(operacoes);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+}
 
 export default OperacaoController;
