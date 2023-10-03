5693caadf1010af704abe2a81b2658fa7945b17c
diasjulia <jedl@cin.ufpe.br>
2023-10-03 16:17:50 -0300

    setup: initial express config
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/index.ts
    
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/index.ts

---
+++ server/src/controllers/OperacaoController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import { Operacao } from "../models/Operacao";
+
+class OperacaoController {}
+
+export default OperacaoController;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,22 @@
+import { Request, Response } from "express";
+import { Application } from "express-serve-static-core";
 import { AppDataSource } from "./data-source";
+import express = require("express");
 
 AppDataSource.initialize()
-  .then(async () => {})
+  .then(async () => {
+    const app: Application = express();
+    const port = 8080;
+
+    app.use(express.json());
+
+    // Rotas da API
+    app.get("/api/ping", (req: Request, res: Response) => {
+      res.json({ message: "Pong!" });
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
   .catch((error) => console.log(error));
