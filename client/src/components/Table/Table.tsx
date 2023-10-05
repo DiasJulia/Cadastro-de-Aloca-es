f1f2ab6d179eb3aefc26c5fdf22614843e68dae4
diasjulia <jedl@cin.ufpe.br>
2023-10-05 16:44:43 -0300

    feat: fetch data about fundos from server

--- client/src/components/Table/Table.tsx
+++ client/src/components/Table/Table.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,25 +8,50 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
+import axios from "axios";
+
+interface Data {
+  CNPJ: string;
+  razao_social: string;
+  quantidade_total: number;
+  preco_total: number;
+  valor_unitario_atual: number;
+}
+
 function createData(
   CNPJ: string,
   razao_social: string,
   quantidade_total: number,
-  valor_total: number,
+  preco_total: number,
   valor_unitario_atual: number
 ) {
   return {
     CNPJ,
     razao_social,
     quantidade_total,
-    valor_total,
+    preco_total,
     valor_unitario_atual,
   };
 }
 
-const rows = [createData("11.511.517/0001-61", "Empresa 1", 1, 1, 1.03)];
-
 function TableComponent() {
+  const [data, setData] = useState<Data[]>([
+    createData("11.511.517/0001-61", "Empresa 1", 1, 1, 1.03),
+  ]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/operacao/grouped").then((response) => {
+      console.log(response.data);
+      var aux: Data[] = [];
+      for (var CNPJ in response.data) {
+        console.log(CNPJ);
+        console.log(response.data[CNPJ]);
+        aux.push(response.data[CNPJ]);
+      }
+      setData(aux);
+    });
+  }, []);
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -42,7 +68,7 @@ function TableComponent() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {data.map((row) => (
             <TableRow
               key={row.CNPJ}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -55,11 +81,11 @@ function TableComponent() {
               <TableCell align="right">{row.valor_unitario_atual}</TableCell>
               <TableCell align="right">{row.quantidade_total}</TableCell>
               <TableCell align="right">
-                {row.valor_total / row.quantidade_total}
+                {row.preco_total / row.quantidade_total}
               </TableCell>
               <TableCell align="right">
                 {row.valor_unitario_atual /
-                  (row.valor_total / row.quantidade_total) -
+                  (row.preco_total / row.quantidade_total) -
                   1}
               </TableCell>
               <TableCell align="right">
