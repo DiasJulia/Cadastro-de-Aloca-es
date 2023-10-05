3002fbea26d1df000128deb73a7fbdddca591dc5
diasjulia <jedl@cin.ufpe.br>
2023-10-05 00:04:23 -0300

    feat: added tests for update and delete functions

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -58,4 +58,59 @@ describe("OperacaoController", () => {
       expect(res.body[0]).toHaveProperty("valor");
     }
   });
+
+  it("should update an operation", async () => {
+    const setupReq = mockRequest({
+      CNPJ: "123456789",
+      razao_social: "Sample Company",
+      tipo: 1,
+      data: "2023-10-03",
+      quantidade: 100,
+      valor: 500.0,
+    });
+    const setupRes = await request(app)
+      .post("/api/operacao")
+      .send(setupReq.body);
+    expect(setupRes.status).toBe(201);
+    const setupId = setupRes.body.id;
+
+    const req = mockRequest({
+      CNPJ: "123456789",
+      razao_social: "Sample Company 2",
+      tipo: 1,
+      data: "2023-10-03",
+      quantidade: 100,
+      valor: 500.0,
+    });
+    const res = await request(app)
+      .put("/api/operacao/" + setupId)
+      .send(req.body);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("CNPJ");
+    expect(res.body).toHaveProperty("razao_social");
+    expect(res.body).toHaveProperty("tipo");
+    expect(res.body).toHaveProperty("data");
+    expect(res.body).toHaveProperty("quantidade");
+    expect(res.body).toHaveProperty("valor");
+    expect(res.body.razao_social).toBe("Sample Company 2");
+  });
+
+  it("should delete an operation", async () => {
+    const setupReq = mockRequest({
+      CNPJ: "123456789",
+      razao_social: "Sample Company",
+      tipo: 1,
+      data: "2023-10-03",
+      quantidade: 100,
+      valor: 500.0,
+    });
+    const setupRes = await request(app)
+      .post("/api/operacao")
+      .send(setupReq.body);
+    expect(setupRes.status).toBe(201);
+    const setupId = setupRes.body.id;
+
+    const res = await request(app).delete("/api/operacao/" + setupId);
+    expect(res.status).toBe(200);
+  });
 });
