d4967bf25e40447cc5dbda772b2b6ff4e35aa492
diasjulia <jedl@cin.ufpe.br>
2023-10-06 19:15:37 -0300

    feat: added form validations

--- client/src/components/RegisterCota/RegisterCota.tsx
+++ client/src/components/RegisterCota/RegisterCota.tsx
@@ -9,16 +9,22 @@ import MenuItem from "@mui/material/MenuItem";
 import "./RegisterCota.css";
 import axios from "axios";
 import CurrencyFormat from "react-currency-format";
+import { FormProvider, useForm } from "react-hook-form";
+import { FormHelperText } from "@mui/material";
 
 function RegisterCota() {
   const [modalOpen, setModalOpen] = useState(false);
-  const [tipo, setTipo] = useState("");
-  const [cnpj, setCnpj] = useState("");
-  const [razaoSocial, setRazaoSocial] = useState("");
+  const [tipo, setTipo] = useState<string | null>("");
+  const [cnpj, setCnpj] = useState<string | null>("");
+  const [razaoSocial, setRazaoSocial] = useState<string | null>("");
   const [quantidade, setQuantidade] = useState<Number | null>();
   const [valorUnitario, setValorUnitario] = useState<Number | null>();
-  const [date, setDate] = useState(new Date(Date.now()));
+  const [date, setDate] = useState<string | null>(
+    new Date(Date.now()).toISOString().split("T")[0]
+  );
   const [inputValue, setInputValue] = useState("" as string);
+
+  const methods = useForm();
 
   const openModal = () => {
     setModalOpen(true);
@@ -34,7 +40,7 @@ function RegisterCota() {
       tipo: tipo,
       CNPJ: cnpj,
       razao_social: razaoSocial,
-      data: date,
+      data: new Date(date ? date : Date.now()),
       quantidade: quantidade,
       valor: valorUnitario,
     };
@@ -55,83 +61,116 @@ function RegisterCota() {
         Adicionar Operação
       </Button>
       <Modal className="form-modal" open={modalOpen} onClose={closeModal}>
-        <FormControl className="register-form">
-          <FormLabel className="form-title">
-            <h3>Adicionar Operação</h3>
-          </FormLabel>
-          <br />
-          <TextField
-            select
-            id="input-tipo"
-            value={tipo}
-            onChange={handleChange}
-            label="Tipo"
-            variant="outlined"
-          >
-            <MenuItem value="COMPRA">Compra</MenuItem>
-            <MenuItem value="VENDA">Venda</MenuItem>
-          </TextField>
-          <br />
-          <TextField
-            id="input-cnpj"
-            label="CNPJ"
-            variant="outlined"
-            value={cnpj}
-            onChange={(e) => setCnpj(e.target.value)}
-          ></TextField>
-          <br />
-          <TextField
-            id="input-razao-social"
-            label="Razão Social"
-            variant="outlined"
-            value={razaoSocial}
-            onChange={(e) => setRazaoSocial(e.target.value)}
-          ></TextField>
-          <br />
-          <TextField
-            type="number"
-            id="input-quantidade"
-            label="Quantidade"
-            variant="outlined"
-            value={quantidade}
-            onChange={(e) => setQuantidade(parseInt(e.target.value))}
-          ></TextField>
-          <br />
-          <CurrencyFormat
-            customInput={TextField}
-            thousandSeparator="."
-            decimalSeparator=","
-            fixedDecimalScale={true}
-            prefix="R$"
-            decimalScale={2}
-            placeholder="R$ 0,00"
-            label="Valor Unitário"
-            value={`R$ ${inputValue}`}
-            onValueChange={(values) => {
-              const { formattedValue, value } = values;
-              setInputValue(formattedValue);
-              setValorUnitario(parseFloat(value));
-            }}
-          />
-          <br />
-          <TextField
-            type="date"
-            id="input-data"
-            label="Data"
-            variant="outlined"
-            value={date.toISOString().split("T")[0]}
-            onChange={(e) => setDate(new Date(e.target.value))}
-          ></TextField>
-          <br />
-          <div className="button-container">
-            <Button variant="contained" color="primary" onClick={submitForm}>
-              Adicionar
-            </Button>
-            <Button variant="outlined" color="error" onClick={closeModal}>
-              Cancelar
-            </Button>
-          </div>
-        </FormControl>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(submitForm)}>
+            <FormControl className="register-form">
+              <FormLabel className="form-title">
+                <h3>Adicionar Operação</h3>
+              </FormLabel>
+              <br />
+              <TextField
+                required
+                select
+                error={tipo === null ? true : false}
+                id="input-tipo"
+                value={tipo}
+                onChange={handleChange}
+                label="Tipo"
+                variant="outlined"
+              >
+                <MenuItem value="COMPRA">Compra</MenuItem>
+                <MenuItem value="VENDA">Venda</MenuItem>
+              </TextField>
+              <br />
+              <TextField
+                required
+                error={cnpj === null ? true : false}
+                id="input-cnpj"
+                label="CNPJ"
+                variant="outlined"
+                value={cnpj}
+                onChange={(e) => setCnpj(e.target.value || null)}
+              ></TextField>
+              <br />
+              <TextField
+                required
+                error={razaoSocial === null ? true : false}
+                id="input-razao-social"
+                label="Razão Social"
+                variant="outlined"
+                value={razaoSocial}
+                onChange={(e) => setRazaoSocial(e.target.value || null)}
+              ></TextField>
+              <br />
+              <TextField
+                required
+                error={quantidade === null ? true : false}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                id="input-quantidade"
+                label="Quantidade"
+                variant="outlined"
+                value={quantidade}
+                onChange={(e) =>
+                  setQuantidade(parseInt(e.target.value) || null)
+                }
+              ></TextField>
+              <br />
+              <CurrencyFormat
+                required
+                error={valorUnitario === null ? true : false}
+                customInput={TextField}
+                thousandSeparator="."
+                decimalSeparator=","
+                fixedDecimalScale={true}
+                prefix="R$"
+                decimalScale={2}
+                placeholder="R$ 0,00"
+                label="Valor Unitário"
+                value={`R$ ${inputValue}`}
+                onValueChange={(values) => {
+                  const { formattedValue, value } = values;
+                  setInputValue(formattedValue);
+                  setValorUnitario(parseFloat(value));
+                }}
+              />
+              <br />
+              <TextField
+                required
+                error={
+                  date === null || new Date(date) > new Date(Date.now())
+                    ? true
+                    : false
+                }
+                type="date"
+                InputProps={{
+                  inputProps: {
+                    min: "2020-05-01",
+                    max: new Date(Date.now()).toISOString().split("T")[0],
+                  },
+                }}
+                id="input-data"
+                label="Data"
+                variant="outlined"
+                value={date}
+                onChange={(e) => setDate(e.target.value || null)}
+              ></TextField>
+              <FormHelperText error={true}>
+                {date === null || new Date(date) > new Date(Date.now())
+                  ? "Data inválida"
+                  : ""}
+              </FormHelperText>
+              <br />
+              <div className="button-container">
+                <Button variant="contained" color="primary" type="submit">
+                  Adicionar
+                </Button>
+                <Button variant="outlined" color="error" onClick={closeModal}>
+                  Cancelar
+                </Button>
+              </div>
+            </FormControl>
+          </form>
+        </FormProvider>
       </Modal>
     </div>
   );
