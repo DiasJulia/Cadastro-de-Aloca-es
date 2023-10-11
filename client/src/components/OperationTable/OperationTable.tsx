67bae53aa2f98d11f4abd3b21cbea50b9b751943
diasjulia <jedl@cin.ufpe.br>
2023-10-11 00:19:55 -0300

    feat: styling filters

--- client/src/components/OperationTable/OperationTable.tsx
+++ client/src/components/OperationTable/OperationTable.tsx
@@ -17,7 +17,7 @@ import "./OperationTable.css";
 import axios from "axios";
 import { OperationForm } from "..";
 import Button from "@mui/material/Button";
-import { Snackbar } from "@mui/material";
+import { Card, FormHelperText, Snackbar, TextField } from "@mui/material";
 
 interface Data {
   id: number;
@@ -53,9 +53,10 @@ function OperationTable() {
   const [modalOpen, setModalOpen] = useState(false);
   const [currentData, setCurrentData] = useState<Data | null>(null);
   const [data, setData] = useState<Data[]>([]);
+  const [currentDataFilter, setCurrentDataFilter] = useState<Data[]>([]);
 
-  const [dataInicio, setDataInicio] = useState<string>("");
-  const [dataFim, setDataFim] = useState<string>("");
+  const [dataInicio, setDataInicio] = useState<string | null>("");
+  const [dataFim, setDataFim] = useState<string | null>("");
 
   const [openDialog, setOpenDialog] = useState(false);
   const [openSnackbar, setOpenSnackbar] = useState(false);
@@ -70,13 +71,14 @@ function OperationTable() {
       return data >= dataInicioDate && data <= dataFimDate;
     });
     console.log(filteredData);
-    setData(filteredData);
+    setCurrentDataFilter(filteredData);
   };
 
   useEffect(() => {
     axios.get("http://localhost:3001/api/operacao").then((response) => {
       console.log(response.data);
       setData(response.data);
+      setCurrentDataFilter(response.data);
     });
   }, []);
 
@@ -94,6 +96,7 @@ function OperationTable() {
       .delete(`http://localhost:3001/api/operacao/${id}`)
       .then((response) => {
         setData(data.filter((row) => row.id !== id));
+        setCurrentDataFilter(data);
         handleCloseDialog();
         setOpenSnackbar(true);
       });
@@ -108,102 +111,142 @@ function OperationTable() {
     setOpenDialog(true);
   };
 
+  const clearFilter = () => {
+    setDataInicio("");
+    setDataFim("");
+    setCurrentDataFilter(data);
+  };
+
   return (
     <>
-      <div>
-        <label>Data de início:</label>
-        <input
-          type="date"
-          value={dataInicio}
-          onChange={(e) => setDataInicio(e.target.value)}
-        />
-        <label>Data de término:</label>
-        <input
-          type="date"
-          value={dataFim}
-          onChange={(e) => setDataFim(e.target.value)}
-        />
-        <button onClick={filterData}>Filtrar</button>
-      </div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>CNPJ</TableCell>
-              <TableCell align="right">Razão Social</TableCell>
-              <TableCell align="right">Data da Operação</TableCell>
-              <TableCell align="right">Tipo</TableCell>
-              <TableCell align="right">Preço</TableCell>
-              <TableCell align="right">Número de cotas</TableCell>
-              <TableCell align="right">Ações</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.CNPJ}
-                </TableCell>
-                <TableCell align="right">{row.razao_social}</TableCell>
-                <TableCell align="right">
-                  {new Date(row.data).toLocaleDateString()}
-                </TableCell>
-                <TableCell align="right">{row.tipo}</TableCell>
-                <TableCell align="right">
-                  {row.valor.toFixed(2).replace(".", ",")}
-                </TableCell>
-                <TableCell align="right">{row.quantidade}</TableCell>
-                <TableCell align="right">
-                  <Edit
-                    color="action"
-                    className="action-button"
-                    onClick={editOperation(row.id)}
-                  />
-                  <Delete
-                    color="action"
-                    className="action-button"
-                    onClick={handleOpenDialog(row.id)}
-                  />
-                </TableCell>
+      <div className="container">
+        <Card className="filter-card">
+          <h4 className="m-1">Filtros</h4>
+          <TextField
+            className="m-1"
+            InputLabelProps={{ shrink: true }}
+            type="date"
+            InputProps={{
+              inputProps: {
+                min: "2020-05-01",
+                max: new Date(Date.now()).toISOString().split("T")[0],
+              },
+            }}
+            id="input-data"
+            label="Data de Início"
+            variant="outlined"
+            value={dataInicio}
+            onChange={(e) => setDataInicio(e.target.value || null)}
+          ></TextField>
+          <TextField
+            className="m-1"
+            InputLabelProps={{ shrink: true }}
+            type="date"
+            InputProps={{
+              inputProps: {
+                min: "2020-05-01",
+                max: new Date(Date.now()).toISOString().split("T")[0],
+              },
+            }}
+            id="input-data"
+            label="Data de Fim"
+            variant="outlined"
+            value={dataFim}
+            onChange={(e) => setDataFim(e.target.value || null)}
+          ></TextField>
+          <div className="d-flex">
+            <Button onClick={clearFilter} variant="outlined" color="error">
+              Limpar
+            </Button>
+            <Button onClick={filterData} variant="contained">
+              Filtrar
+            </Button>
+          </div>
+        </Card>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>CNPJ</TableCell>
+                <TableCell align="right">Razão Social</TableCell>
+                <TableCell align="right">Data da Operação</TableCell>
+                <TableCell align="right">Tipo</TableCell>
+                <TableCell align="right">Preço</TableCell>
+                <TableCell align="right">Número de cotas</TableCell>
+                <TableCell align="right">Ações</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <OperationForm
-        modalOpen={modalOpen}
-        closeModal={() => setModalOpen(false)}
-        currentData={currentData}
-      />
-      <Dialog
-        open={openDialog}
-        onClose={handleCloseDialog}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Você tem certeza que deseja apagar essa operação?
-            <br />
-            Essa ação não pode ser desfeita.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog}>Cancelar</Button>
-          <Button onClick={deleteOperation(currentId!)} autoFocus color="error">
-            Apagar
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={6000}
-        onClose={() => setOpenSnackbar(false)}
-        message="Operação apagada com sucesso"
-      />
+            </TableHead>
+            <TableBody>
+              {currentDataFilter.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.CNPJ}
+                  </TableCell>
+                  <TableCell align="right">{row.razao_social}</TableCell>
+                  <TableCell align="right">
+                    {new Date(row.data).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell align="right">{row.tipo}</TableCell>
+                  <TableCell align="right">
+                    {row.valor.toFixed(2).replace(".", ",")}
+                  </TableCell>
+                  <TableCell align="right">{row.quantidade}</TableCell>
+                  <TableCell align="right">
+                    <Edit
+                      color="action"
+                      className="action-button"
+                      onClick={editOperation(row.id)}
+                    />
+                    <Delete
+                      color="action"
+                      className="action-button"
+                      onClick={handleOpenDialog(row.id)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <OperationForm
+          modalOpen={modalOpen}
+          closeModal={() => setModalOpen(false)}
+          currentData={currentData}
+        />
+        <Dialog
+          open={openDialog}
+          onClose={handleCloseDialog}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Você tem certeza que deseja apagar essa operação?
+              <br />
+              Essa ação não pode ser desfeita.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog}>Cancelar</Button>
+            <Button
+              onClick={deleteOperation(currentId!)}
+              autoFocus
+              color="error"
+            >
+              Apagar
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Snackbar
+          open={openSnackbar}
+          autoHideDuration={6000}
+          onClose={() => setOpenSnackbar(false)}
+          message="Operação apagada com sucesso"
+        />
+      </div>
     </>
   );
 }
