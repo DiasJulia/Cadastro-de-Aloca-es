8d2cd3a7a4e607447a9d79557891a753ca2beb76
diasjulia <jedl@cin.ufpe.br>
2023-10-17 14:33:10 -0300

    setup: env variables

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        SERVER_HOST_EXTERNAL: process.env.SERVER_HOST_EXTERNAL
+    }
+}
 
 module.exports = nextConfig
