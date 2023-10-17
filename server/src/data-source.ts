608a4f91a29a498a250ac6aa2e137a30a8914e24
diasjulia <jedl@cin.ufpe.br>
2023-10-17 11:02:28 -0300

    setup: env variables

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.HOST_INTERNAL || "postgres",
+  host: process.env.HOST_INTERNAL_DEV || "postgres",
   port: 5432,
-  username: process.env.POSTGRESQL_LOGIN || "postgres",
-  password: process.env.POSTGRESQL_PASSWORD || "123456",
-  database: process.env.DEFAULT_DATABASE_NAME || "database",
+  username: process.env.POSTGRESQL_LOGIN_DEV || "postgres",
+  password: process.env.POSTGRESQL_PASSWORD_DEV || "123456",
+  database: process.env.DEFAULT_DATABASE_NAME_DEV || "database",
   synchronize: true,
   logging: false,
   entities: ["src/models/*.ts"],
